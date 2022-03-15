dddbdb9ab94d9f8831437987f16e770a065dfb2f
PEDRO4LBERTO <pedroalbertosdias@gmail.com>
2022-03-15 23:20:38 +0000

    Aula 2 (15/03)

---
+++ Aula2/Aula2.js
@@ -0,0 +1,9 @@
+/* Para criar um aquivo node eu devo acessar:
+1- Acessar o CMD
+2- cd dir
+3- Acessar a pasta onde quero criar meu arquivo
+4- cd Desktop
+5- cd Ensino/Fatec
+6- criação do projeto: npx create-next-app projeto001
+7- devo entrar dentro da pagina para rodar o arquivo ex: cd C:\Users\1090482023012\Desktop\Ensino\fatec\projeto001
+8- e por ultimo inserir o comando: npm run dev */
